3e8f606959df17a96067760af22ec56e9bd6095c
Andrey Ermilov <andreiermilov@bk.ru>
2024-05-13 12:34:04 +0300

    Test ci

--- tests/test_popups.py
+++ tests/test_popups.py
@@ -1,4 +1,5 @@
 from pages.popups import Popups
+import pytest
 
 
 def test_alert_popup_button(browser):
@@ -6,3 +7,8 @@ def test_alert_popup_button(browser):
     page.open()
     page.button_alert_click()
     assert page.alert_text == 'Hi there, pal!'
+
+
+@pytest.mark.regress
+def test_ci():
+    assert 2 == 2
