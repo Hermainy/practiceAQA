ebb839e0994c44021f4ca1d8274ebbdbc72c86fb
Andrey Ermilov <andreiermilov@bk.ru>
2024-05-13 13:09:04 +0300

    update fixture

--- conftest.py
+++ conftest.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 
@@ -10,10 +8,8 @@ import pytest
 @pytest.fixture()
 def browser():
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('window-size=1920x1080')
+    options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    service = Service(ChromeDriverManager(driver_version="124.0.6367.202").install())
-    chrome_browser = webdriver.Chrome(service=service, options=options)
+    chrome_browser = webdriver.Chrome(options=options)
     return chrome_browser
