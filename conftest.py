fe9ad925a6483d4495d9f9eb65ec17e144ffa2f1
Andrey Ermilov <andreiermilov@bk.ru>
2024-05-13 12:47:42 +0300

    add headless option

--- conftest.py
+++ conftest.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
 
 import pytest
 
 
 @pytest.fixture()
 def browser():
-    service = Service(ChromeDriverManager(driver_version="124.0.6367.202").install())
+    options = Options()
+    options.add_argument('--headless')
+    service = Service(ChromeDriverManager(driver_version="124.0.6367.202").install(), chrome_options=options)
     chrome_browser = webdriver.Chrome(service=service)
     return chrome_browser
