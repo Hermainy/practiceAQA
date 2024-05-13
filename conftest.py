36a4703539971a718b65328d0be9d93483f3384f
Andrey Ermilov <andreiermilov@bk.ru>
2024-05-13 13:15:36 +0300

    update fixture

--- conftest.py
+++ conftest.py
@@ -8,7 +8,8 @@ import pytest
 @pytest.fixture()
 def browser():
     options = Options()
-    options.add_argument('--window-size=1920x1080')
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--disable-gpu')
     options.add_argument('--start-maximized')
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
