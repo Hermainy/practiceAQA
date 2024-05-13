c862027c7d176855b55f66a274433c7df7c2a261
Andrey Ermilov <andreiermilov@bk.ru>
2024-05-13 13:10:48 +0300

    update fixture

--- conftest.py
+++ conftest.py
@@ -8,7 +8,9 @@ import pytest
 @pytest.fixture()
 def browser():
     options = Options()
-    options.add_argument('--headless=new')
+    options.add_argument('--window-size=1920x1080')
+    options.add_argument('--start-maximized')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     chrome_browser = webdriver.Chrome(options=options)
